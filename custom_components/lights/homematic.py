c71b94947e46d033c1224d245e30bb7ee5c0e013
jazzaj <jazzaj@users.noreply.github.com>
2016-04-01 20:45:59 +0200

    Implementation of event callbacks

--- custom_components/lights/homematic.py
+++ custom_components/lights/homematic.py
@@ -1,119 +1,128 @@
 import logging
 
 """
-The homematic custom switch platform.
+The homematic custom light platform.
+Will also work with switches.
 
 Configuration:
 
-# TODO
+light:
+  - platform: homematic
+    key: '<Homematic key for device>' # e.g. 'JEQ0XXXXXXX'
+    name: '<User defined name>'
 
 """
 
 from homeassistant.components.light import (ATTR_BRIGHTNESS, Light)
+from homeassistant.const import (STATE_ON, STATE_OFF)
 
 _LOGGER = logging.getLogger(__name__)
 
 # List of component names (string) your component depends upon.
 DEPENDENCIES = ['homematic']
-
 REQUIREMENTS = ['pyhomematic']
 
 import pyhomematic
+#import homematic as pyhomematic
 
 HOMEMATIC = pyhomematic
 
-def setup_platform(hass, config, add_devices, discovery_info=None):
+def setup_platform(hass, config, add_callback_devices, discovery_info=None):
     if HOMEMATIC.Server is None:
         _LOGGER.error('Homematic server not initialized')
         return False
     key = config.get('key', None)
     name = config.get("name", None)
-    dimmer = str(config.get("dimmer", False)).upper() == "TRUE"
     if key is None:
         _LOGGER.warning('Homematic light: Key missing in configuration file')
         return False
-    add_devices([HMLight(key, name, dimmer, hass)])
+    add_callback_devices([HMLight(key, name)])
     return True
 
 
 class HMLight(Light):
     """Represents an Homematic Switch in Home Assistant."""
     
-    def __init__(self, key, name, dimmer, hass):
+    def __init__(self, key, name):
         """Initialize an Homematic Light."""
-		self.entity_id = key
-        self._key = key # TODO: Maybe not required and can be replaced by entity_id (previous line)
+        self._key = key
         self._name = name
-        self._dimmer = dimmer
-        self._hass = hass
-        if not hasattr(HOMEMATIC.devices[self._key], 'level'):
-            self._dimmer = False
-        self._is_available = True
-        HOMEMATIC.devices[self._key].set_state_changed_callback(homematic_changed_state)
+        if hasattr(HOMEMATIC.devices[self._key], 'level'):
+            self._dimmer = True 
+            self._level = HOMEMATIC.devices[self._key].level
+        else:
+            self._dimmer = False 
+            self._state = HOMEMATIC.devices[self._key].is_on
+        
+        def event_received(device, caller, attribute, value):
+            attribute = str(attribute).upper()
+            if attribute == 'LEVEL':
+                self._level = float(value)
+            elif attribute == 'STATE':
+                self._state = bool(value)
+            elif attribute == 'UNREACH':
+                self._is_available = not bool(value)
+            else:
+                return
+            self.update_ha_state()
+            
+        HOMEMATIC.devices[self._key].setEventCallback(event_received)
+        self._is_available = self._check_availability()
         
     @property
     def should_poll(self):
         """Return True if entity has to be polled for state.
         False if entity pushes its state to HA.
         """
-		# TODO: Check if this is required after callback logic is implemented
-        return True
-	
-	@property
+        return False
+    
+    @property
     def name(self):
-        """Return the hostname of the server."""
+        """Return the name of the light."""
         return self._name
     
     @property
     def brightness(self):
-        """Return the brightness of this light between 0..1"""
+        """Return the brightness of this light between 0..255"""
         if self._dimmer:
-            return int(HOMEMATIC.devices[self._key].level*255)
+            return int(self._level * 255)
         else:
-            if self.is_on:
-                return 255
-            else:
-                return 0
+            return None
     
     @property
     def is_on(self):
-        """Return True if entity is on."""
+        """Return True if light is on."""
         if self._dimmer:
-            return HOMEMATIC.devices[self._key].level > 0
+            return self._level > 0
         else:
-            return HOMEMATIC.devices[self._key].is_on
-			
-	@property
+            return self._state
+
+    @property
     def assumed_state(self):
-        """Return True if unable to access real state of the entity."""
-		# TODO: Return True if homematic has communication issues with the device.
-        return False
-		
-	@property
+        """Return True if unable to access real state of the light."""
+        return not self.available
+        
+    @property
     def available(self):
-        """Return True if entity is available."""
-		# TODO: Return False if homematic has communication issues with the device.
-        return True
-		
+        """Return True if light is available."""
+        return self._is_available
+        
     def turn_on(self, **kwargs):
-        """Turn the entity on."""
+        """Turn the light on."""
         if ATTR_BRIGHTNESS in kwargs and self._dimmer:
             percent_bright = float(kwargs[ATTR_BRIGHTNESS]) / 255
             HOMEMATIC.devices[self._key].level = percent_bright
         else:
-            HOMEMATIC.devices[self._key].on()            
+            self._state = True
+            HOMEMATIC.devices[self._key].on()
 
     def turn_off(self, **kwargs):
-        """Turn the entity off."""
+        """Turn the light off."""
+        if not self._dimmer:
+            self._state = False
         HOMEMATIC.devices[self._key].off()
-        
-    def homematic_changed_state(self, hm_new_state):
-        # TODO: Convert homematic new_state to HASS new state
-		hass_new_state = {
-            "last_updated": now,
-            "state": "on",
-            "entity_id": e_id,
-            "attributes": {},
-            "last_changed": now
-        }
-        self._hass.states.set(self.entity_id, hass_new_state, attributes=None)
+
+    def _check_availability(self):
+        """ Test if device is available. Only required at startup as later the availabilty will be tracked by monitoring events."""
+        # TODO: Missing/unknow functionality in pyhomematic
+        return True
