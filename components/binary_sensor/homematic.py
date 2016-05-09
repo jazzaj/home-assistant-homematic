f6c9b48316fd327c422c3922eb547ee9c72dcefa
jazzaj <jazzaj@users.noreply.github.com>
2016-05-09 23:03:42 +0200

    Remote: renamed channel to button

--- components/binary_sensor/homematic.py
+++ components/binary_sensor/homematic.py
@@ -70,11 +70,13 @@ class HMBinarySensor(homematic.HMDevice, BinarySensorDevice):
                 else:
                     self._battery = 4.6
             elif attribute == 'PRESS_LONG_RELEASE':
-                if int(device.split(':')[1]) == int(self._channel):
+                if int(device.split(':')[1]) == int(self._button):
                         self._state = 0
             elif attribute == 'PRESS_SHORT' or attribute == 'PRESS_LONG':
-                if int(device.split(':')[1]) == int(self._channel):		
+                if int(device.split(':')[1]) == int(self._button):        
                     self._state = 1
+                    self.update_ha_state()
+                    self._state = 0
             elif attribute == 'UNREACH':
                 self._is_available = not bool(value)
             else:
@@ -89,9 +91,9 @@ class HMBinarySensor(homematic.HMDevice, BinarySensorDevice):
                 self._state = self._hmdevice.state
         if type(self._hmdevice).__name__ == "HMRemote":
             self._sensor_class = 'remote button'
-            self._channel = self._config.get('channel', None)
-            if not self._channel:
-                _LOGGER.error("No channel defined for '%s'" %self._address)
+            self._button = self._config.get('button', None)
+            if not self._button:
+                _LOGGER.error("No button defined for '%s'" %self._address)
                 self._is_available = False
         if self._is_available:
             self._hmdevice.setEventCallback(event_received)
